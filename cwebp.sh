6f84b413e122fa8a7d668b472039a716f3c3c808
dansemal <dansemalrayo@gmail.com>
2021-12-26 14:48:04 +0800

    111

--- cwebp.sh
+++ cwebp.sh
@@ -1,7 +1,16 @@
-images=$(ls -d 4 *.png)
+cd /root/picture_2
+git pull
+images=$(ls  *.png)
 
 for image in $images
 do
   # -q 90 指定转换质量，经试验这个值效果和压缩率都不错
   cwebp $image -q 70 -o ${image%.*}.webp
+  rm -rf $image
 done
+date=`date "+%Y-%m-%d %H:%M:%S"`
+
+#提交
+git add .
+git commit -m "图片优化$(date)"
+git push
